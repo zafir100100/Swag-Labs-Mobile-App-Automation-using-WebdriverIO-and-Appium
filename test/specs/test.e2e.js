c77e397a4908bd8b13947d4515f0a3f7bd0f3c9b
Zafir <55599023+zafir100100@users.noreply.github.com>
2024-11-12 16:47:54 +0600

    Update test.e2e.js

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,19 +2,19 @@ const LoginPage = require('../pageobjects/login.page')
 const ProductsPage = require('../pageobjects/products.page')
 
 describe('Regression Test On Swag Labs Mobile App', () => {
-    // it('should display locked-out error for locked-out user login attempt', async () => {
-    //     await LoginPage.selectLockedOutUser();
-    //     await LoginPage.submitLogin();
-    //     await LoginPage.validateErrorMessage();
-    // });
+    it('should display locked-out error for locked-out user login attempt', async () => {
+        await LoginPage.selectLockedOutUser();
+        await LoginPage.submitLogin();
+        await LoginPage.validateErrorMessage();
+    });
     it('should successfully log in with valid standard user credentials', async () => {
         await LoginPage.selectStandardUser();
         await LoginPage.submitLogin();
         await LoginPage.validateSuccessfulLogin();
     });
     it('should add product to cart and verify button change to remove', async () => {
-        // await ProductsPage.clickFilterButton();
-        // await ProductsPage.selectFilterOption();
+        await ProductsPage.clickFilterButton();
+        await ProductsPage.selectFilterOption();
         await ProductsPage.validateFirstProduct();
     });
     it('should add to cart product and verify cart badge is changed', async () => {
